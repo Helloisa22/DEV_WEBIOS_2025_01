e358d986a78b8882cee13f4a8892d161a6d54d44
Helloisa22 <helloisa.betriz44@gmail.com>
2025-06-03 21:39:57 -0300

    Aula JS 01 nao finalizada

---
+++ JS/JS01-Introducao/main.js
@@ -0,0 +1,32 @@
+var idade
+const ano = 2010
+// JavaScript
+console.log("Hello world") // apresentar uma mensagem
+console.clear() // limpar
+
+// Variaveis
+var numero1 = 192 // inicializar uma variavel
+console.log(numero1)
+
+var num1 = 4
+var num2 = 4
+var resultado = num1 + num2
+console.log(resultado)
+
+idade = 89 // iniciar a variavel em outra parte do codigo
+console.log(idade)
+
+ano = 1936
+const PI = 3.14
+
+// Textos - Strings
+let nome = 'Matheus Araujo'
+const nome2 = "Matheus Vinicius"
+var nome3 = "12365.4789" // texto
+console.log(nome, nome2, nome3)
+
+VAR // Flexivel e com menos regras (Posso alterar o valor inicial de uma var e recriar a mesma em outras partes do codigo)
+
+LET // Você não pode recriar a mesma em outras partes do codigo com o mesmo nome, mas pode chamar e alterar o valor inicial
+
+CONST // Você não pode recriar a mesma em outras partes do cogido e não pode alterar o valor inicial dela garantindo a confiança do dado que está sendo utilizado no decorrer do seu codigo
