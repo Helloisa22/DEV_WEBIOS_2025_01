4c6f1955e589c5d21f5706b3c68f5540885a65ba
Helloisa22 <helloisa.betriz44@gmail.com>
2025-06-25 21:26:37 -0300

    regra prjeto

---
+++ main.js
@@ -0,0 +1,16 @@
+// Campo para receber a zona que a pessoa mora
+let campoZona = 'ZonaLeste'
+// Campo para receber a quantidade de dormitorios > 1 ou 2 dormitorios
+let qtdDormitorio = 1
+// Campo para receber a quantidade de pessoas que serão financiadoras 1 até  5 pessoas
+let qtdPessoas = 1
+// Campo para receber valor da renda bruta familiar
+let rendaBrutaFamiliar = 3800
+// Campo para receber o valor total do FGTS de todos os familiares
+let valorTotalFGTS = 4200
+
+/*
+    REGRAS PARA A ESCOLHA DA PORCENTAGEM DE ACORDO O VALOR DO SALARIO BRUTO
+
+
+*/
