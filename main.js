19f0ccfa7d0ee1537d7e3b31c97b371da74376b9
Helloisa22 <helloisa.betriz44@gmail.com>
2025-06-26 21:24:40 -0300

    PROJETO

--- main.js
+++ main.js
@@ -8,9 +8,36 @@ let qtdPessoas = 1
 let rendaBrutaFamiliar = 3800
 // Campo para receber o valor total do FGTS de todos os familiares
 let valorTotalFGTS = 4200
+// Campo para guardar a porcentaem do financiamento
+let porcentagemDesconto = 0
 
 /*
     REGRAS PARA A ESCOLHA DA PORCENTAGEM DE ACORDO O VALOR DO SALARIO BRUTO
 
-
+    renda em 1.509 até 3018 > 30% ?? 80%
+    renda em 3019 até 4.527 > 60% ?? 60%
+    renda em 4.527 até 7.545 > 30% ?? 30%
 */
+
+rendaBrutaFamiliar
+
+
+// function EscolheZona(){
+
+// }
+
+// function EscolhaQtdDormitorio(){
+
+// }
+
+// function EscolheQtdPessoas(){
+
+// }
+
+// function RendaTotalFamiliar(){
+
+// }
+
+// function FuncValorTotalFGTS(){
+
+// }
